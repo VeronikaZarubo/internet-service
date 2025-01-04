ce2ca4d54ca271b099543bf087219d2f69393904
Veronika Zarubo <nikazarubo1@gmail.com>
2025-01-04 17:34:37 +0100

    Log In

--- server.js
+++ server.js
@@ -37,24 +37,71 @@ app.use(function (req, res, next) {
     try{
         const decoded = jwt.verify(req.cookies.ourSimpleApp, process.env.JWTSECRET)
         req.user = decoded
-    }
-    catch(err){
+    }    catch(err){
         req.user = false
     }
 
     res.locals.user = req.user
-    console.log(req.user);
-    
+    console.log(req.user);    
 
     next()
 })
 
 app.get("/", (req, res) => {
+    if (req.user){
+        return res.render("dashboard")
+    }
     res.render("homepage")
 })
 
 app.get("/login", (req, res) => {
     res.render("login")
+})
+
+app.get("/logout", (req, res) =>{
+    res.clearCookie("ourSimpleApp")
+    res.redirect("/")
+})
+
+app.post("/login", (req, res) =>{
+    let errors = []
+  
+    if (typeof req.body.username !== "string") req.body.username = ""
+    if (typeof req.body.password !== "string") req.body.password = ""
+    
+    if (req.body.username.trim() =="") errors = ["Invalid username / password"]
+    if (req.body.password =="") errors = ["Invalid username / password"]
+
+    if (errors.length){
+        return res.render("login", {errors})
+    }
+
+    const userInQuestionStatement = db.prepare("SELECT * FROM users WHERE USERNAME = ?")
+    const userInQuestion = userInQuestionStatement.get(req.body.username)
+
+    if (!userInQuestion) {
+        errors = ["Invalid username / password."]
+        return res.render("login", { errors })
+    }
+
+    const matchOrNot = bcrypt.compareSync(req.body.password, userInQuestion.password)
+    if (!matchOrNot) {
+        errors = ["Invalid username / password."]
+        return res.render("login", { errors })
+    }
+
+    const ourTokenValue = jwt.sign(
+        { exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24, skyColor: "blue", userid: userInQuestion.id, username: userInQuestion.username },
+        process.env.JWTSECRET
+    )
+
+    res.cookie("ourSimpleApp", ourTokenValue, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 1000 * 60 * 60 * 24})
+
+    res.redirect("/")
 })
 
 app.post("/register", (req, res) => {
@@ -69,6 +116,17 @@ app.post("/register", (req, res) => {
     if (req.body.username && req.body.username.length < 3) errors.push("Username must be at least 3 characters.")
     if (req.body.username && req.body.username.length > 10) errors.push("Username cannot exceed 10 characters.")
     if (req.body.username && !req.body.username.match(/^[a-zA-Z0-9]+$/)) errors.push("Username can only contain letters and numbers.")
+
+    //check if username exists
+    const usernameStatement = db.prepare("SELECT * FROM users WHERE username = ?")
+    const usernameCheck = usernameStatement.get(req.body.username)
+
+    if (usernameCheck) errors.push("That username is already taken.")
+
+        if (!req.body.password) errors.push("You must provide a password.")
+            if (req.body.password && req.body.password.length < 12) errors.push("Password must be at least 12 characters.")
+            if (req.body.password && req.body.password.length > 70) errors.push("Password cannot exceed 70 characters.")
+          
     
     if (errors.length) {
         return res.render("homepage", {errors})
@@ -97,7 +155,7 @@ app.post("/register", (req, res) => {
         maxAge: 1000 * 60 * 60 * 24
     })
 
-    res.send("Thank you!")
+    res.redirect("/")
 })
 
 app.listen(3000)
